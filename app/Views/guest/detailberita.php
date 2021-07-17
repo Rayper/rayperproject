42ef934426cfa8f1df83a6ef39512335f83e9f7d
Sahlan Fawaid <lannfawaid@gmail.com>
2021-07-17 15:36:59 +0700

    Bug Fix Session Guest Detail Berita

--- app/Views/guest/detailberita.php
+++ app/Views/guest/detailberita.php
@@ -62,7 +62,7 @@
                 </div>
                 <p class="my-3"><?= $detailberita['Isi']; ?></p>
                 <br>
-                <a href="/member/liatberita">
+                <a href="/Guest/liatberita">
                     << Kembali ke Halaman Berita</a> </div> </section> </main> <!-- Footer -->
                         <?= $this->include('templates/footerguest'); ?>
 
